82a43b4a076301e8764098aa0bf9a535df874ef5
arieltapialv <ariel.tapia@lateralview.net>
2016-12-28 10:17:05 -0300

    in progress searchFeature

--- src/home/home.js
+++ src/home/home.js
@@ -2,9 +2,10 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import component from './home.component';
 import albumItem from './albumItem/albumItem';
+import searchFeature from './searchFeature/searchFeature';
 
 const homeMain = angular
-  .module('homeMain', [uiRouter, albumItem])
+  .module('homeMain', [uiRouter, albumItem, searchFeature])
   .component('home', component)
   .config(($stateProvider) => {
     'ngInject';
